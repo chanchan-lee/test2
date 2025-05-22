0da309d1aee4ca99ce075e697f2c61abb6e81238
chanchan-lee <chanchan@titaninvest.co.kr>
2025-05-22 10:59:03 +0900

    init

---
+++ src/main/java/org/example/test22/Test22Application.java
@@ -0,0 +1,13 @@
+package org.example.test22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test22Application.class, args);
+    }
+
+}
